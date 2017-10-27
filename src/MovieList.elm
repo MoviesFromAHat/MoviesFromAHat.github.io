d2e38c1dbc04af1192a019b282472bcb6c3bfc57
Segobiano <missionmojo@gmail.com>
2017-10-27 09:25:30 -0400

    2 additional movies  (#23)
    * 3 new movies
    
    * The other two movies
    
    * added more to the genres

--- src/MovieList.elm
+++ src/MovieList.elm
@@ -951,5 +951,21 @@ movies =
       , genres = [ "drama", "comedy" ]
       , watched = Unwatched
       }
+    , { title = "Son of Rambow"
+      , url = "http://www.imdb.com/title/tt0845046/"
+      , img = "son-of-rambow.jpg"
+      , year = 2007
+      , runtime = 95
+      , genres = [ "comedy", "adventure" ]
+      , watched = Unwatched
+      }
+    , { title = "My Blue Heaven"
+      , url = "http://www.imdb.com/title/tt0100212/"
+      , img = "my-blue-heaven.jpg"
+      , year = 1990
+      , runtime = 97
+      , genres = [ "comedy", "action" ]
+      , watched = Unwatched
+      }
     ]
         |> List.map (\m -> { m | genres = Genre.fromFlatList m.genres })
